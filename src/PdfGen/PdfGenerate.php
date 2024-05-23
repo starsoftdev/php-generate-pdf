d34451a711306f7e5fe8f0a3d5d83e0ab8475483
SoftwareKings <dragonfighter0120@gmail.com>
2024-05-23 11:51:13 -0400

    save pdf to local sever

--- src/PdfGen/PdfGenerate.php
+++ src/PdfGen/PdfGenerate.php
@@ -5,12 +5,21 @@ namespace PdfGen;
 use Spiritix\Html2Pdf\Converter;
 use Spiritix\Html2Pdf\Input\StringInput;
 use Spiritix\Html2Pdf\Output\DownloadOutput;
+use Spiritix\Html2Pdf\Output\FileOutput;
 
 class PdfGenerate {
         public function generate($formData) {
 
-            // print_r($formData);
-            // exit;
+        $outputDir = 'output';
+        $outputFilePath = $outputDir . '/deploma_001.pdf';
+
+        // Create the output directory if it doesn't exist
+        if (!is_dir($outputDir)) {
+            if (!mkdir($outputDir, 0777, true)) {
+                die('Failed to create folders...');
+            }
+        }
+
         $doc = new \DOMDocument();
         $doc->loadHTMLFile(__DIR__. "/../files/estacio.html");
         
@@ -37,19 +46,12 @@ class PdfGenerate {
 
         $outputHtml = $doc->saveHTML();
 
-        // $pdf = new TCPDF();
-        // $pdf->AddPage();
-        // $pdf->writeHTML($outputHtml, true, false, true, false, '');
-
-        // $pdf->Output('hello_world.pdf');
-
 
         $input = new StringInput();
         $input->setHtml($outputHtml);
 
-        $converter = new Converter($input, new DownloadOutput());
+        $converter = new Converter($input, new FileOutput());
 
-        // $converter->setOption('landscape', true);
 
         $converter->setOptions([
             'printBackground' => true,
@@ -68,9 +70,48 @@ class PdfGenerate {
 
         $output = $converter->convert();
         
-        $output->download('deploma_001.pdf');
+        $filePath = "output/deploma_001.pdf";
+        try {
+            $output->store($filePath);
+        } catch(Exception $e) {
+            echo "File is already existed";
+        }
 
+
+        if(!file_exists($filePath)){ // file does not exist
+            die('file not found');
+        } else {
+            header("Cache-Control: public");
+            header("Content-Description: File Transfer");
+            header("Content-Disposition: attachment; filename=deploma_001.pdf");
+            header("Content-Type: application/zip");
+            header("Content-Transfer-Encoding: binary");
+
+            // read the file from disk
+            readfile($filePath);
+        }
+
+        // $converter1 = new Converter($input, new FileOutput());
         
-        echo "Hello World";
+
+        // $converter1->setOptions([
+        //     'printBackground' => true,
+        //     'displayHeaderFooter' => true,
+        //     'landscape' => true,
+        //     // 'width' => '32.6898cm',
+        //     // 'height' => '21.971cm',
+        //     'preferCSSPageSize' => true,
+        //     'margin' => array(
+        //         'top' => '0',
+        //         'bottom' => '0',
+        //         'left' => '0',
+        //         'right' => '0',
+        //     )
+        // ]);
+
+        // $output1 = $converter->convert();
+        // $output1->download("deploma_001.pdf");
+        
+        echo "File is successfully created.";
     }
 }
